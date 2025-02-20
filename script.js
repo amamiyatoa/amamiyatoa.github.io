3a1efa7097ff4617d86b9517e8bff98c9a3092da
amamiyatoa <hrt.mr@icloud.com>
2025-02-20 23:23:31 +0900

    js追加とフェードイン導入

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-function displayTime() {
-    const date = new Date();
-    const month = date.getMonth();
-    const day =  date.getDate();
-    const hour = date.getHours();
-    const minute =  date.getMinutes();
-    const second = date.getSeconds();
-    const year = date.getFullYear();
-    const currentTime = `${year}/${month}/${day}/${hour}:${minute}:${second}`;
-    document.querySelector('.clock').textContent = currentTime;
+window.onload = function() {
+    let fade_menu = document.getElementById('menu');
+    fade_menu.addEventListener('mouseover', function() {
+        if(fade_menu.className != 'fadein_active') {
+            fade_menu.classList.add('fadein_active');
+        }
+    });
+    fade_menu.addEventListener('mouseout', function() {
+        if(fade_menu.className = 'fadein_active') {
+            fade_menu.classList.remove('fadein_active');
+        }
+    });
 }
-setInterval(
-    displayTime,1000
-)
