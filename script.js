3b7174b62238500bab809dcb16a4e0d98473b955
amamiyatoa <hrt.mr@icloud.com>
2025-02-21 14:45:40 +0900

    jsの修正

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 window.onload = function() {
+    //フェードイン導入JavaScript//
     let fade_menu = document.getElementById('menu');
     fade_menu.addEventListener('mouseover', function() {
         if(fade_menu.className != 'fadein_active') {
             fade_menu.classList.add('fadein_active');
         }
-    });
-    fade_menu.addEventListener('mouseout', function() {
-        if(fade_menu.className = 'fadein_active') {
-            fade_menu.classList.remove('fadein_active');
-        }
+        fade_menu.addEventListener('mouseout', function() {
+            if(fade_menu.className = 'fadein_active') {
+                fade_menu.classList.remove('fadein_active');
+            }
+        });
     });
 }
